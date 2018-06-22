596851d186797156b97f51e6e6a84d9e723f6100
cheng-qian-123 <cheng_qian_123@163.com>
2018-06-22 09:35:42 +0800

    第二次提交

--- src/Test01.java
+++ src/Test01.java
@@ -2,4 +2,8 @@
  * created by chengqian on 2018/6/21
  */
 public class Test01 {
+
+    public void go(){
+        System.out.println("hehe");
+    }
 }
---
+++ src/Test02.java
@@ -0,0 +1,5 @@
+/**
+ * created by chengqian on 2018/6/21
+ */
+public class Test02 {
+}
